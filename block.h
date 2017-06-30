6c0fe616939d52fb51b0541beaa516d0e84373a8
NeeXxx <neexgzyx@163.com>
2017-06-30 21:53:29 +0800

    a

--- block.h
+++ block.h
@@ -5,6 +5,7 @@ enum underSubstance{water,house,ground,tree,bridge};//枚举的顺序和数组�
 enum aboveSubstance{sBomb,arrowUp,arrowDown,arrowLeft,arrowRight,
                     player1,player2,
                     wood,air};
+enum texture{t_nothing,t_tree,t_house,t_player1,t_player2,t_wood,t_bomb,t_flame};
 
 //希望底部的东西是不可改变的
 class under
